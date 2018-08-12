a24b6b6bccee2a866ca798542846b919b0127e86
Junwoong <chawnsnd@gmail.com>
2018-08-12 19:31:12 +0900

    로그인 관련 초기세팅

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 var db = require('../models/db');
 require('../models/user');
 var User = db.model('User');
 var router = express.Router();
 
+//내정보 불러오기
 router.get('/me', (req, res, next)=>{
     if(!req.user) return res.json({success: false})
     res.json({success: true, me: req.user})
 })
 
+//회원가입
 router.post('/', (req, res, next) => {
     if(!req.body.email) return res.json({success: false, message: '`email`는 필수 파라미터 입니다.'});
 	if(!req.body.duckname) return res.json({success: false, message: '`duckname`는 필수 파라미터 입니다.'});
@@ -39,6 +42,7 @@ router.post('/', (req, res, next) => {
     })
 })
 
+//가입 시 중복체크
 router.post('/verify', (req, res, next) => {
     var email = req.body.email;
     var duckname = req.body.duckname;
@@ -85,4 +89,37 @@ router.post('/verify', (req, res, next) => {
     }
 })
 
+//로그인
+router.post('/login', (req, res, next) => {
+	User.findOne({email: req.body.email}, (err, user) => {
+		if(err) return next(err);
+		if(!user) return res.json({success: false, message: '아이디를 찾을 수 없습니다.'});
+
+		const hash = crypto.pbkdf2Sync(req.body.password, user.salt, 8192, 24, 'sha256').toString('base64');
+		if(hash != user.password) return res.json({success: false, message: '비밀번호가 일치하지 않습니다.'});
+
+		if(user.status == 'GENERAL' || user.status == 'ADMIN') {
+			jwt.sign({
+				user_id: user._id
+			}, req.app.get('jwt-secret'), {
+				expiresIn: '1y',
+				issuer: 'nizduck.com',
+				subject: 'user_id'
+			}, (err, token) => {
+				if(req.body.keep) {
+					res.cookie('token', token, {httpOnly: true, expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 12)});
+				} else {
+					res.cookie('token', token, {httpOnly: true});
+				}
+				return res.json({success: true, token});
+			});
+		} else if(user.status == 'DENIED') {
+			return res.json({});
+		} else if(user.status == 'WAITING') {
+			return res.json({});
+		} else if(user.status == 'LEAVE') {
+			return res.json({});
+		}
+	});
+});
 module.exports = router;
